d9a95f3b57e5d1a09f076cbc37b36597395780c0
DESKTOP-KNEF2HR <stranniknochi@mail.ru>
2022-12-25 11:33:08 +0800

    first commit

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] maxtrix = new int[5, 5];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
+
+FillArray(maxtrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+}
+
+if (n > maxtrix.GetLength(0) || m > maxtrix.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine("значение элемента равно = " + maxtrix[n - 1, m - 1]);
+}
+
+PrintArray(maxtrix);
---
+++ Zadacha52/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int [,] maxtrix = new int [3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i, j] = new Random().Next(0, 20);
+        }
+    }
+}
+
+FillArray (maxtrix);
+PrintArray(maxtrix);
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+
+FillArray(maxtrix);
+for (int j = 0; j < maxtrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < maxtrix.GetLength(0); i++)
+    {
+        avarage = (avarage + maxtrix[i,j]);
+    }
+    avarage = avarage / 4;
+    Console.Write(avarage + "; ");
+}
